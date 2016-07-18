768180dea54b5ccddfb124e4b075baa38dfd355b
thu <mingfei.net@gmail.com>
2016-07-18 16:32:04 +0800

    Update.

--- sql/day1.sql
+++ sql/day1.sql
@@ -23,3 +23,61 @@ FROM db_test.table_test;
 DELETE FROM db_test.table_test;
 
 TRUNCATE TABLE db_test.table_test;
+
+CREATE TABLE db_test.sc (
+  id    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  sid   INT UNSIGNED,
+  cid   INT UNSIGNED,
+  grade TINYINT UNSIGNED
+);
+
+INSERT INTO db_test.sc VALUES (NULL, 1, 1, 60);
+INSERT INTO db_test.sc VALUES (NULL, 1, 2, 60);
+INSERT INTO db_test.sc VALUES (NULL, 2, 2, 60);
+SELECT *
+FROM db_test.sc;
+
+-- table student 从表 子表
+DROP TABLE IF EXISTS db_test.student;
+CREATE TABLE db_test.student (
+  id            INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  sno           CHAR(8),
+  name          VARCHAR(20),
+  age           INT,
+  department_id INT UNSIGNED,
+  FOREIGN KEY (department_id) REFERENCES db_test.department (id)
+);
+
+# ALTER TABLE db_test.student
+#   ADD CONSTRAINT fk_student_department_id
+# FOREIGN KEY (department_id)
+# REFERENCES db_test.department (id);
+
+-- table department 主表 父表
+DROP TABLE IF EXISTS db_test.department;
+CREATE TABLE db_test.department (
+  id   INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20),
+  tel  VARCHAR(20)
+);
+
+INSERT INTO db_test.student VALUES (NULL, '20160001', 'zhangsan', 18, 1);
+INSERT INTO db_test.student VALUES (NULL, '20160002', 'lisi', 18, 1);
+INSERT INTO db_test.student VALUES (NULL, '20160003', 'wanger', 18, 2);
+INSERT INTO db_test.student VALUES (NULL, '20160004', 'tester', 18, 3);
+
+INSERT INTO db_test.department VALUES (NULL, 'CS', '62780001');
+INSERT INTO db_test.department VALUES (NULL, 'EE', '62780002');
+
+SELECT *
+FROM db_test.student;
+
+SELECT *
+FROM db_test.department;
+
+SELECT
+  s.name,
+  d.name,
+  d.tel
+FROM db_test.student s, db_test.department d
+WHERE s.department_id = d.id;

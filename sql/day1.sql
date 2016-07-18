cd22e62fd1b60a7fbe2a563aaf7386ca7ba1e5c5
thu <mingfei.net@gmail.com>
2016-07-18 15:11:25 +0800

    Update.

--- sql/day1.sql
+++ sql/day1.sql
@@ -6,16 +6,20 @@ CREATE DATABASE db_test;
 DROP TABLE IF EXISTS db_test.table_test;
 CREATE TABLE db_test.table_test (
   id     INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 主关键字 主键 主码 primary key
-  name   VARCHAR(255),
-  age    INT(2),
+  name   VARCHAR(255) NOT NULL,
+  age    INT(2) UNSIGNED ZEROFILL,
   views  INT UNSIGNED,
   height DOUBLE(8, 6),
   price  DECIMAL(6, 2),
-  dob    DATE, -- date of birth
+  dob    DATE UNIQUE, -- date of birth
   time   DATETIME
 );
 
-INSERT INTO db_test.table_test VALUES (NULL, '张三', 20, 100, 1.71, 5000.5, '1990-6-9', '1999-9-9 13:25:59');
+INSERT INTO db_test.table_test VALUES (NULL, 'zhangsan', 2, 100, 1.71, 5000.5, NULL, '1999-9-9 13:25:59');
 
 SELECT *
 FROM db_test.table_test;
+
+DELETE FROM db_test.table_test;
+
+TRUNCATE TABLE db_test.table_test;

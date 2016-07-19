1e3eb6c50c6a2be53c71db048ba7aadf2fcb1121
thu <mingfei.net@gmail.com>
2016-07-19 10:20:55 +0800

    Update.

--- sql/day2.sql
+++ sql/day2.sql
@@ -7,17 +7,20 @@ SHOW TABLES;
 -- table employee
 DROP TABLE IF EXISTS db_test.employee;
 CREATE TABLE db_test.employee (
-  id           INT UNIQUE  AUTO_INCREMENT PRIMARY KEY
+  id           INT UNSIGNED AUTO_INCREMENT PRIMARY KEY
   COMMENT 'PK',
   ename        VARCHAR(255) COMMENT '姓名',
-  departmentId INT UNSIGNED COMMENT '部门编号 FK',
-  FOREIGN KEY (departmentId) REFERENCES db_test.department (id)
+  departmentId INT UNSIGNED COMMENT '部门编号 FK' -- ,
+  #   FOREIGN KEY (departmentId) REFERENCES db_test.department (id)
 )
   COMMENT '员工表';
 
 INSERT INTO db_test.employee VALUES (NULL, 'e1', 1);
 INSERT INTO db_test.employee VALUES (NULL, 'e2', 2);
 INSERT INTO db_test.employee VALUES (NULL, 'e3', NULL);
+
+DELETE FROM db_test.employee
+WHERE id = 12;
 
 SELECT *
 FROM db_test.employee;
@@ -36,3 +39,15 @@ INSERT IGNORE db_test.department VALUES (NULL, 'd2');
 
 SELECT *
 FROM db_test.department;
+
+DELETE FROM db_test.department
+WHERE id = 1;
+
+-- FK
+ALTER TABLE db_test.employee
+  ADD CONSTRAINT
+FOREIGN KEY (departmentId)
+REFERENCES db_test.department (id)
+  ON DELETE CASCADE; -- cascading style sheet CSS
+
+SET FOREIGN_KEY_CHECKS = 1;

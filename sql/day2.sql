5d43cf9a213c969aac9937d741297ee6ac5d2d5c
thu <mingfei.net@gmail.com>
2016-07-19 09:18:35 +0800

    Update.

---
+++ sql/day2.sql
@@ -0,0 +1,38 @@
+SHOW DATABASES;
+DROP DATABASE IF EXISTS db_test;
+CREATE DATABASE db_test;
+USE db_test;
+SHOW TABLES;
+
+-- table employee
+DROP TABLE IF EXISTS db_test.employee;
+CREATE TABLE db_test.employee (
+  id           INT UNIQUE  AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  ename        VARCHAR(255) COMMENT '姓名',
+  departmentId INT UNSIGNED COMMENT '部门编号 FK',
+  FOREIGN KEY (departmentId) REFERENCES db_test.department (id)
+)
+  COMMENT '员工表';
+
+INSERT INTO db_test.employee VALUES (NULL, 'e1', 1);
+INSERT INTO db_test.employee VALUES (NULL, 'e2', 2);
+INSERT INTO db_test.employee VALUES (NULL, 'e3', NULL);
+
+SELECT *
+FROM db_test.employee;
+
+-- table department
+DROP TABLE IF EXISTS db_test.department;
+CREATE TABLE db_test.department (
+  id    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  dname VARCHAR(255) COMMENT '部门名'
+)
+  COMMENT '部门表';
+
+INSERT IGNORE db_test.department VALUES (NULL, 'd1');
+INSERT IGNORE db_test.department VALUES (NULL, 'd2');
+
+SELECT *
+FROM db_test.department;

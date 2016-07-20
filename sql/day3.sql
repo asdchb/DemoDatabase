717421e0459e62e0a19133ce82e7407d922397ea
thu <mingfei.net@gmail.com>
2016-07-20 11:12:56 +0800

    Update.

--- sql/day3.sql
+++ sql/day3.sql
@@ -7,4 +7,32 @@ SHOW TABLES;
 
 SELECT *
 FROM db_sample.customers
-ORDER BY cust_name DESC , cust_email; -- ASC
+ORDER BY cust_name DESC, cust_email; -- ASC
+
+SELECT *
+FROM db_sample.products
+WHERE vend_id NOT IN ('dll01', 'brs01');
+
+SELECT *
+FROM db_sample.products
+WHERE prod_price NOT BETWEEN 5.99 AND 8.99; -- [5, 10]
+
+SELECT prod_name '商品 名称'
+FROM db_sample.products;
+
+SELECT p.prod_name
+FROM db_sample.products p;
+
+SELECT p.prod_name
+FROM db_sample.products p;
+
+SELECT *
+FROM db_sample.customers
+WHERE cust_email IS NULL;
+
+UPDATE db_sample.customers
+SET cust_email = NULL
+WHERE cust_id = '1000000001';
+
+DESC db_sample.customers;
+
